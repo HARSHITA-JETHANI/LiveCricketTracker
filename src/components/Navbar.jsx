f40260b15253609e396a770aa53bd5ebf4bf4c02
HARSHITA-JETHANI <harshitajethani29@gmail.com>
2026-06-09 01:34:53 +0530

    Create reusable navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header className="mb-8">
+      <h1 className="text-4xl font-bold">
+        Live Cricket Dashboard
+      </h1>
+
+      <p className="text-gray-600 mt-2">
+        Track ongoing cricket matches.
+      </p>
+    </header>
+  );
+}
+
+export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,11 @@
+import Navbar from "../components/Navbar";
 import MatchCard from "../components/MatchCard";
 import matches from "../data/matches";
 
 function HomePage() {
   return (
     <div className="p-6">
-      <header className="mb-8">
-        <h1 className="text-4xl font-bold">
-          Live Cricket Dashboard
-        </h1>
-
-        <p className="text-gray-600 mt-2">
-          Track ongoing cricket matches.
-        </p>
-      </header>
+      <Navbar />
 
       <div className="grid gap-4 md:grid-cols-2">
         {matches.map((match) => (
